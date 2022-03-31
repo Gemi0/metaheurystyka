28c1635ab3bc1cd9bf6fd3ddd7300393b9aecc5d
Dominik Gorgosch <dominik.gch@gmail.com>
2022-03-31 13:21:39 +0200

    Small bugfix

--- src/main/java/algorithms/KRandom.java
+++ src/main/java/algorithms/KRandom.java
@@ -8,11 +8,17 @@ import java.util.List;
 import java.util.Random;
 
 public class KRandom {
-    public Tour kRandom(TSPData data, int k) {
+    /**
+     * Generating the shortest tour using k random tours and picking the shortest.
+     * @param data distance matrix
+     * @param k number of tours generated
+     * @return shortest tour generated
+     */
+    public Tour kRandom(TSPData data, int k) { //O(k) * O(n) = O(n)
         Random rand = new Random();
         int n = data.distance.length;
         Tour best = null;
-        for (int a =0; a <=k; a++) {
+        for (int a =0; a <=k; a++) { // O(k)
             List<Integer> arr = new ArrayList<>(n);
             for (int i = 0; i < n; i++) {
                 arr.add(i);
@@ -22,7 +28,7 @@ public class KRandom {
             int tmp;
 
             //shuffle above input array
-            for (int i = n; i > 0; i--) {
+            for (int i = n; i > 0; i--) { //O(n)
                 r = rand.nextInt(i);
 
                 tmp = arr.get(i - 1);
--- src/main/java/algorithms/Neighbour.java
+++ src/main/java/algorithms/Neighbour.java
@@ -7,18 +7,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Neighbour {
-    public Tour neighbour(TSPData data, int start) {
+    public Tour neighbour(TSPData data, int start) { //O(n)*O(n) = O(n^2)
         int n = data.distance.length;
         List<Integer> arr = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { //O(n)
             arr.add(i);
         }
         List<Integer> tourArr = new ArrayList<>(n);
         tourArr.add(start);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { //O(n)
             double min = 99999999;
             int minPos = -1;
-            for (int j : arr) {
+            for (int j : arr) { //O(n)
                 if (data.distance[start][j] < min && data.distance[start][j] != -1) {
                     min = data.distance[start][j];
                     minPos = j;
