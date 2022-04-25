d45e6de9621a1562bad242ae836b9b7440ac63fe
Gemi0 <dominik.gch@gmail.com>
2022-04-25 22:13:43 +0200

    Added more neighbourhoods and anti - stagnation methods

--- src/main/java/algorithms/TabuSearch.java
+++ src/main/java/algorithms/TabuSearch.java
@@ -7,7 +7,9 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class TabuSearch {
 
+    static final int stopVariable = 1000;
     static final int tabuListSize = 7;
+    static final int maxStagnation = 100;
     static int withoutUpgrade;
     static int iteration;
     static long start;
@@ -21,8 +23,8 @@ public class TabuSearch {
         return iteration < maxIterations;
     }
 
-    private static boolean timeStop(long maxTime) {
-        return end - start < maxTime;
+    private static boolean timeStop(int maxTime) {
+        return end - start < (long)maxTime;
     }
 
     public static int[] tabuSearch(TSPData data, int[] startPermutation) {
@@ -33,13 +35,15 @@ public class TabuSearch {
         int[] neighbourhoodBestPermutation = currentPermutation;
 
         TabuList<Solution> tabuList = new TabuList<>(tabuListSize);
+        BacktrackList<Solution> backtrackList = new BacktrackList<>(101);
+        TabuList<Solution> stagnationTabuList = new TabuList<>(tabuListSize);
 
         double neighbourhoodBestPermutationValue;
         withoutUpgrade = 0;
         iteration = 0;
 
         start = System.currentTimeMillis();
-        while (withoutUpgrade < 10000) {
+        while (timeStop(stopVariable)) {
             Solution bestSolution = null;
             neighbourhoodBestPermutationValue = Double.MAX_VALUE;
             for (int i = 0; i < data.distance.length; i++) {
@@ -63,10 +67,12 @@ public class TabuSearch {
                 }
             }
             if (bestSolution != null) {
+                backtrackList.offer(bestSolution);
                 if (tabuList.size() == tabuListSize) {
                     tabuList.poll();
                 }
                 tabuList.offer(bestSolution);
+
             }
             if (neighbourhoodBestPermutationValue < bestPermutationValue) {
                 bestPermutationValue = neighbourhoodBestPermutationValue;
@@ -76,9 +82,29 @@ public class TabuSearch {
             else {
                 withoutUpgrade++;
             }
+            if (withoutUpgrade < 8) {
+                stagnationTabuList.offer(bestSolution);
+            }
+
+            currentPermutation = neighbourhoodBestPermutation;
+            if (withoutUpgrade == maxStagnation) { //anti - stagnation
+                currentPermutation = bestPermutation;
+                tabuList.elements = stagnationTabuList.elements;
+            }
+            /*if (withoutUpgrade == maxStagnation) { //Backtracking that goes back to the best solution. Possible change to go just few steps back
+                Solution solution;
+                TabuList<Solution> tempTabuList = new TabuList<>(tabuListSize);
+                while (Arrays.equals(currentPermutation, bestPermutation)) {
+                    solution = backtrackList.poll();
+                    tempTabuList.offer(solution);
+                    currentPermutation = invert(currentPermutation, solution.x, solution.y);
+                }
+                for(int i = 0; i < tempTabuList.size(); i++) {
+                    tabuList.offer(tempTabuList.poll());
+                }
+            }*/
             iteration++;
             end = System.currentTimeMillis();
-            currentPermutation = neighbourhoodBestPermutation;
         }
         return bestPermutation;
     }
@@ -94,6 +120,71 @@ public class TabuSearch {
         return temp;
     }
 
+    private static int[] swap(int[] currentPermutation, int i, int j) {
+        int[] temp = currentPermutation.clone();
+        int tempElem = temp[i];
+        temp[i] = temp[j];
+        temp[j] = tempElem;
+        return temp;
+    }
+
+    private static int[] insert(int[] currentPermutation, int i, int j) {
+        int[] temp = currentPermutation.clone();
+        int tempElem = temp[i];
+        if (j - i >= 0) System.arraycopy(temp, i + 1, temp, i, j - i);
+        temp[j] = tempElem;
+        return temp;
+    }
+
+    private static class BacktrackList<T> extends ArrayBlockingQueue<T> {
+
+        private final LinkedList<T> elements;
+
+        public BacktrackList(int capacity) {
+            super(capacity);
+            this.elements = new LinkedList<>();
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return elements.iterator();
+        }
+
+        @Override
+        public int size() {
+            return elements.size();
+        }
+
+        @Override
+        public boolean offer(T t) {
+            if(t == null) return false;
+            elements.add(t);
+            return true;
+        }
+
+        @Override
+        public T poll() {
+            Iterator<T> iter = elements.iterator();
+            T t = iter.next();
+            T t2 = t;
+            while(true){
+                t2 = t;
+                try {
+                    t = iter.next();
+                }
+                catch (NoSuchElementException e ) {
+                    iter.remove();
+                    return t2;
+                }
+            }
+        }
+
+        @Override
+        public T peek() {
+            return elements.getFirst();
+        }
+    }
+
     private static class TabuList<T> extends ArrayBlockingQueue<T> {
 
         public TabuList(int capacity) {
@@ -103,7 +194,7 @@ public class TabuSearch {
 
         private final HashSet<T> hashSet = new HashSet<>();
 
-        private final LinkedList<T> elements;
+        private LinkedList<T> elements;
 
         public boolean isTabu(T t) {
             return hashSet.contains(t);
