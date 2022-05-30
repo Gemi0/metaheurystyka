e098930915b48f0eca0cad37f4cb75e3b8ccc346
Dominik Gorgosch <dominik.gch@gmail.com>
2022-05-30 21:44:48 +0200

    Fixed GeneticScout

--- src/main/java/algorithms/ABC/Bees/Scouts/GeneticScout.java
+++ src/main/java/algorithms/ABC/Bees/Scouts/GeneticScout.java
@@ -2,6 +2,7 @@ package algorithms.ABC.Bees.Scouts;
 
 import algorithms.ABC.ArtificialBeeColony;
 import algorithms.ABC.Flower;
+import main.Loader;
 import main.TSPData;
 
 import java.util.*;
@@ -53,10 +54,16 @@ public class GeneticScout extends Scouts{
         Integer[] flower1Copy = Arrays.stream(flower1Arr).boxed().toArray( Integer[]::new );
         Map<Integer, Integer> map1 = new TreeMap<>();
         Map<Integer, Integer> map2 = new TreeMap<>();
+        Map<Integer, Integer> map1c = new TreeMap<>();
+        Map<Integer, Integer> map2c = new TreeMap<>();
         Random rnd = new Random();
 
-        int pivot1 = rnd.nextInt(flower1Arr.length - 2);
-        int pivot2 = rnd.nextInt(pivot1 + 1, flower1Arr.length);
+        //int pivot1 = rnd.nextInt(flower1Arr.length - 2);
+        //int pivot2 = rnd.nextInt(pivot1 + 1, flower1Arr.length);
+        int pivot1 = 2;
+        int pivot2 = 5;
+        System.out.println(pivot1);
+        System.out.println(pivot2);
         int[] arr1 = new int[flower1Arr.length];
         int[] arr2 = new int[flower1Arr.length];
 
@@ -94,31 +101,91 @@ public class GeneticScout extends Scouts{
         int[] count = new int[arr1.length];
         int[] count2 = new int[arr1.length];
         for(int i = 0; i < arr1.length; i++) {
-            if(count[arr1[i]] == 0) {
-                count[arr1[i]]++;
-            }
-            else {
+
+            count[arr1[i]]++;
+            if(count[arr1[i]] > 1) {
                 Optional<Integer> firstKey = map1.keySet().stream().findFirst();
-                if (firstKey.isPresent()) {
-                    Integer key = firstKey.get();
-                    arr1[i] = map1.get(key);
-                    map1.remove(key);
+                if (i < pivot1 || i > pivot2) {
+                    if (firstKey.isPresent()) {
+                        count[arr1[i]]--;
+                        Integer key = firstKey.get();
+                        arr1[i] = map1.get(key);
+                        map1.remove(key);
+                    }
+                }
+                else {
+                    if (firstKey.isPresent()) {
+                        Integer key = firstKey.get();
+                        int val = map1.get(key);
+                        map1.remove(key);
+                        map1c.put(key, val);
+                    }
                 }
             }
 
-            if(count2[arr2[i]] == 0) {
-                count2[arr2[i]]++;
-            }
-            else {
+
+            count2[arr2[i]]++;
+            if(count2[arr2[i]] > 1) {
                 Optional<Integer> firstKey = map2.keySet().stream().findFirst();
-                if (firstKey.isPresent()) {
-                    Integer key = firstKey.get();
-                    arr2[i] = map2.get(key);
-                    map2.remove(key);
+                if (i < pivot1 || i > pivot2) {
+                    if (firstKey.isPresent()) {
+                        count2[arr2[i]]--;
+                        Integer key = firstKey.get();
+                        arr2[i] = map2.get(key);
+                        map2.remove(key);
+                    }
+                }
+                else {
+                    if (firstKey.isPresent()) {
+                        Integer key = firstKey.get();
+                        int val = map2.get(key);
+                        map2.remove(key);
+                        map2c.put(key, val);
+                    }
                 }
             }
         }
+
+        for(int i = 0; i < arr1.length; i++) {
+            if(count[arr1[i]] > 1) {
+                if (i < pivot1 || i > pivot2) {
+                    Optional<Integer> firstKey = map1c.keySet().stream().findFirst();
+                    if (firstKey.isPresent()) {
+                        Integer key = firstKey.get();
+                        arr1[i] = map1c.get(key);
+                        map1c.remove(key);
+                    }
+                }
+            }
+
+            if(count2[arr2[i]] > 1) {
+                if (i < pivot1 || i > pivot2) {
+                    Optional<Integer> firstKey = map2c.keySet().stream().findFirst();
+                    if (firstKey.isPresent()) {
+                        Integer key = firstKey.get();
+                        arr2[i] = map2c.get(key);
+                        map2c.remove(key);
+                    }
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
         flower1.setPermutation(arr1);
         flower2.setPermutation(arr2);
+    }
+
+    public static void main(String[] args) {
+        String distanceFilePath = System.getProperty("user.home") + "\\IdeaProjects\\metaheurystyka\\src\\main\\java\\data\\symetric\\berlin52.xml";
+        String coordsFilePath = System.getProperty("user.home") + "\\IdeaProjects\\metaheurystyka\\src\\main\\java\\data\\symetric\\coords\\berlin52.tsp";
+        TSPData data = Loader.loadWithCoords(distanceFilePath, coordsFilePath);
+
+        int[] a = {0,1,2,3,4,5,6,7};
+        int[] b = {7,6,3,4,1,0,2,5};
+        Flower flower1 = new Flower(a, data);
+        Flower flower2 = new Flower(b, data);
+
+        PMXCrossbreeding(flower1, flower2);
     }
 }
